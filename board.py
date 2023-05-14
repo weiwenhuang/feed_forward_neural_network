6a98b646af28c2404019d4e34f70c590c70d7e73
Weiwen Huang <56329298+weiwenhuang@users.noreply.github.com>
2023-05-14 17:49:24 -0400

    update

--- board.py
+++ board.py
@@ -1,14 +1,20 @@
+import copy
 
-def TOMOVE(state):
+
+#return movable chess pieces
+#return type: arraylist
+def TOMOVE(state,player):
     res =  []
     for i in range(len(state)):
         for j in range(len(state[0])):
-            if state[i][j] == 1:
+            if state[i][j] == player:
                 res.append([i,j])
     return res
 
-def ACTIONS(state):
-    queue = TOMOVE(state)
+#return all posibble move for player
+#return type: hashmap
+def ACTIONS(state,player):
+    queue = TOMOVE(state,player)
     res = {}
     while queue:
         index = queue.pop(0)
@@ -20,30 +26,55 @@ def ACTIONS(state):
                 else:
                     res[strindex].append([index[0]-1,index[1]])
             if index[1]-1 in range(0,len(state[0])):
-                if state[index[0]-1][index[1]-1] == -1:
+                if state[index[0]-1][index[1]-1] == player*-1:
                     if strindex not in res:
                         res[strindex] = [[index[0]-1,index[1]-1]]
                     else:
                         res[strindex].append([index[0]-1,index[1]-1])
             if index[1]+1 in range(0,len(state[0])):
-                if state[index[0]-1][index[1]+1] == -1:
+                if state[index[0]-1][index[1]+1] == player*-1:
                     if strindex not in res:
                         res[strindex] = [[index[0]-1,index[1]+1]]
                     else:
                         res[strindex].append([index[0]-1,index[1]+1])
     return res
 
+#implement the action
+#return type array list
+def RESULT(origion,act):
+    state = copy.deepcopy(origion)
+    s = state[act[0][0]][act[0][1]] 
+    state[act[0][0]][act[0][1]] = 0
+    state[act[1][0]][act[1][1]] = s
+    return state
 
+#check is thhe state terminal
+#return type Boolean
+def IS_TERMINAL(state,player):
+    if not TOMOVE(state,player):
+        return True
+    else:
+        return False
+
+#change array to str
 def tostr(arr):
     if len(arr) == 2:
         return str(arr[0]) +','+ str(arr[1])
     else:
         return print("arr lenth must = 2")
+
+#change str to arr
+def toarr(str):
+    arr = str.split(',')
+    return [int(arr[0]),int(arr[1])]
     
 
 def main():
-    board = [[-1,-1,-1],[0,-1,0],[1,1,1]]
-    print(ACTIONS(board))
+    board = [[-1,-1,-1],[0,0,0],[1,1,1]]
+    acthash = ACTIONS(board,1)
+    act = [[2,0],[1,0]]
+    newboard = RESULT(board,act)
+
 
 
 if __name__ == "__main__":
