9f3b78bfb1bcee2e84a6d93189bdd7d755abc708
Weiwen Huang <56329298+weiwenhuang@users.noreply.github.com>
2023-05-18 18:48:07 -0400

    ud

--- cccc.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-aaa= np.array([1,2,3])
-print(aaa[-3])
---
+++ main.py
@@ -0,0 +1,9 @@
+import cnn
+
+def main():
+
+    #cnn.addtest()
+    cnn.defhexapawntest()
+    #cnn.testweight()
+if __name__ == "__main__":
+    main()
