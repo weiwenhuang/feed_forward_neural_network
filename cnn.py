3d1a3c500796288f8ad010d2b7b841e3dcbce50b
Weiwen Huang <56329298+weiwenhuang@users.noreply.github.com>
2023-05-18 18:45:51 -0400

    ud

--- cnn.py
+++ cnn.py
@@ -1,7 +1,52 @@
 import numpy as np
+import data as da
+import board as bd
+
 np.set_printoptions(suppress=True)
 def main():
     #choose,input_units,hidden_layers,hidden_units,output_units
+    #defhexapawntest()
+    defhexapawntest()
+
+def defhexapawntest():
+    a = Neuron('sigmoid',10,2,2,1)
+    all_y_trues = np.array([])
+    tran_data = []
+    for i in da.boards :
+        all_y_trues =  np.append(all_y_trues,bd.minmax(i,1))
+        tran_data.append(toarr(i))
+    a.update_weight(tran_data,all_y_trues)
+    print(a.feedforward([1, -1, -1, 0, 0, 0, -1, 1, 1, 1]))
+
+
+
+def toarr(x):
+    res = [1]
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            res.append(x[i][j])
+    return res
+
+def testweight():
+    a = Neuron('sigmoid',2,2,2,1)
+    data = np.array([
+    [-2, -1],  
+    [25, 6],   
+    [17, 4],   
+    [-15, -6], 
+    ])
+    all_y_trues = np.array([
+    1, 
+    0, 
+    0, 
+    1, 
+    ])
+
+    a.update_weight(data,all_y_trues)
+    print(a.feedforward(np.array([25,6])))
+
+def addtest():
+
     a = Neuron('sigmoid',2,2,2,2)
 
 
@@ -18,9 +63,9 @@ def main():
     [0,1], 
     [1,0], 
     ])
-
     a.update_weight(data,all_y_trues)
     print(a.feedforward(np.array([1,1])))
+
 
 def function(str,x):
     if str == 'Relu':
@@ -70,7 +115,7 @@ class Neuron:
         tem_weight = np.random.randn(self.hidden_units, self.output_units)
         tem_bias = np.random.randn(self.output_units, 1)
         self.layers_level.append([tem_weight, tem_bias])
-        print(self.layers_level)
+        #print(self.layers_level)
     
     def feedforward(self,x):
 
@@ -104,7 +149,7 @@ class Neuron:
     
     def update_weight(self,data, ally_true):
         learn_rate = 0.1
-        epochs = 1 # number of times to loop through the entire dataset
+        epochs = 1000 # number of times to loop through the entire dataset
         for epoch in range(epochs):
             for x, y_true in zip(data, ally_true):
                 predicty = self.feedforward(x)
@@ -120,18 +165,18 @@ class Neuron:
                             #self.layers_level[-i][0][j][k] -= learn_rate * d_L_d_ypred * h[k] * deriv_sigmoid(self.forward_prevalue[-i][k])
                             for index in range(len(self.layers_level[-i][1])):
                                 #weight
-                                if k == index:
-                                    self.layers_level[-i][0][j][k] -= learn_rate * d_L_d_ypred[index] * h[k] * deriv_sigmoid(self.forward_prevalue[-i][k])
-                                #bias
-                                self.layers_level[-i][1][index] -= learn_rate * d_L_d_ypred[index] * deriv_sigmoid(np.float64(self.forward_prevalue[-i][k]))
-                                #learn_rate * d_L_d_ypred * deriv_sigmoid(np.float64(self.forward_prevalue[-i][k]))
-                            #self.layers_level[-i][1] -= learn_rate * d_L_d_ypred * deriv_sigmoid(self.forward_prevalue[-i][k])
+                                for index2 in range(len(d_L_d_ypred)):
+                                    if k == index:
+                                        self.layers_level[-i][0][j][k] -= learn_rate * d_L_d_ypred[index2] * h[k] * deriv_sigmoid(self.forward_prevalue[-i][k])
+                                    #bias
+                                    self.layers_level[-i][1][index] -= learn_rate * d_L_d_ypred[index2] * deriv_sigmoid(np.float64(self.forward_prevalue[-i][k]))
+
                     
 
             if epoch % 10 == 0:
                 y_preds = np.apply_along_axis(self.feedforward, 1, data)
                 loss = mse_loss(ally_true, y_preds)
-                print("Epoch %d loss: %.3f" % (epoch, loss))
+                print("Epoch %d learning rate: %.3f" % (epoch, loss))
 
 
 if __name__ == "__main__":
