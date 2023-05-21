c7376a4f3dc70b27983810c7af210614bc5cf6f1
Weiwen Huang <56329298+weiwenhuang@users.noreply.github.com>
2023-05-21 01:21:47 -0400

    ud

--- cnn.py
+++ cnn.py
@@ -31,24 +31,24 @@ def toarr(x):
 
 def testweight():
     #choose,input_units,hidden_layers,hidden_units,output_units
-    a = Neuron('sigmoid',2,2,3,1)
+    a = Neuron('sigmoid',2,2,2,1)
     data = np.array([
     [-2, -1],  
-    #[25, 6],   
-    #[17, 4],   
-    #[-15, -6], 
+    [25, 6],   
+    [17, 4],   
+    [-15, -6], 
     ])
     all_y_trues = np.array([
     1, 
-    #0, 
-    #0, 
-    #1, 
+    0, 
+    0, 
+    1, 
     ])
     a.update_weight(data,all_y_trues)
     emily = np.array([-7, -3]) # 128 pounds, 63 inches
-    #frank = np.array([20, 2])  # 155 pounds, 68 inches
+    frank = np.array([20, 2])  # 155 pounds, 68 inches
     print("Emily: %.3f" % a.feedforward(emily)) # 0.951 - F
-    #print("Frank: %.3f" % a.feedforward(frank)) # 0.039 - M
+    print("Frank: %.3f" % a.feedforward(frank)) # 0.039 - M
 
 def addtest():
 
@@ -118,7 +118,7 @@ class Neuron:
             tem_bias = np.random.randn(n, 1)
             self.layers_level.append([tem_weight, tem_bias])
         #predict
-        tem_weight = np.random.randn(self.hidden_units, self.output_units)
+        tem_weight = np.random.randn(self.output_units, self.hidden_units)
         tem_bias = np.random.randn(self.output_units, 1)
         self.layers_level.append([tem_weight, tem_bias])
         print("------NETWAREK LAYERS-------")
@@ -171,14 +171,33 @@ class Neuron:
             self.forward_sum_value.append(arr1)
             self.forward_pre_value.append(arr2)
 
-        #outputlayer:
+        #outputlayer: 
+        '''
+        if self.output_units == 1:
+            arr1 = []
+            arr2 = []
+            for i in range(self.output_units):
+                sum_value = 0
+                for j in range(self.hidden_units):
+                    #print('x:',self.forward_pre_value[-1][j],'weight:',self.layers_level[-1][0][j])
+                    print('--L:',sum_value ,self.forward_pre_value[-1][j],self.layers_level[-1][0][i][j])
+                    sum_value += self.forward_pre_value[-1][j] * self.layers_level[-1][0][i][j]
+                print('bias',self.layers_level[-1][1][i])
+                sum_value += self.layers_level[-1][1][i]
+                #print('bias:',self.layers_level[-1][1][i])
+                arr1.append(sum_value)
+                #get predict value fron function
+                predict = sigmoid(sum_value)
+                arr2.append(predict)
+            self.forward_sum_value.append(arr1)
+            self.forward_pre_value.append(arr2)'''
         arr1 = []
         arr2 = []
         for i in range(self.output_units):
             sum_value = 0
             for j in range(self.hidden_units):
-                #print('x:',self.forward_pre_value[-1][j],'weight:',self.layers_level[-1][0][j])
-                sum_value += self.forward_pre_value[-1][j] * self.layers_level[-1][0][j]
+                #print('--L:',sum_value ,self.forward_pre_value[-1][j],self.layers_level[-1][0][i][j])
+                sum_value += self.forward_pre_value[-1][j] * self.layers_level[-1][0][i][j]
             sum_value += self.layers_level[-1][1][i]
             #print('bias:',self.layers_level[-1][1][i])
             arr1.append(sum_value)
@@ -189,24 +208,57 @@ class Neuron:
         self.forward_pre_value.append(arr2)
         return predict
     
-    
     def update_weight(self,data, ally_true):
+
+            # weight  self.layers_level[i][0]   bias self.layers_level[i][1]
+
         learn_rate = 0.1
-        epochs = 1 # number of times to loop through the entire dataset
+        epochs = 1000 # number of times to loop through the entire dataset
         for epoch in range(epochs):
             for x, y_true in zip(data, ally_true):
-                print(x,y_true)
                 predicty = self.feedforward(x)
+                '''
+                print('---------------weight------------------')
+                for i in range(len(self.layers_level)):
+                    print(self.layers_level[i][0])
+                print('---------------bias------------------')
+                for i in range(len(self.layers_level)):
+                    print(self.layers_level[i][1])
+                print('---------------sum------------------')
+                print(self.forward_sum_value)
+                print('---------------predict------------------')
+                print(self.forward_pre_value)
+                '''
                 d_L_d_ypred = -2 * (y_true - predicty)
                 
-                
+                # last layer
+                for j in range(self.output_units):
+                    for i in range(len(self.forward_pre_value[-2])):
+                        #print(self.forward_pre_value[-2][i],self.forward_pre_value[-1][j])
+                        d_ypred_d_w = self.forward_pre_value[-2][i] * deriv_sigmoid(self.forward_sum_value[-1][j])
+                        #weight update
+                        self.layers_level[-1][0][j][i] -= learn_rate * d_L_d_ypred * d_ypred_d_w
+                    d_ypred_d_b = deriv_sigmoid(self.forward_sum_value[-1][j])
+                    # bias update
+                    self.layers_level[-1][1][j] -= learn_rate * d_L_d_ypred * d_ypred_d_b
 
-                    
+                # rest layer
+                for k in range(len(self.layers_level)-1,0, -1):
+                    for j in range(self.hidden_units):
+                        for i in range(len(self.forward_pre_value[k-1])):
+                            #print('sss',self.forward_pre_value[k-1][i],self.forward_sum_value[k][j])
+                            d_ypred_d_w = self.forward_pre_value[k-1][i] * deriv_sigmoid(self.forward_sum_value[k][j])
+                            #print('checkweight',self.layers_level[k-1][0][j][i])
+                            self.layers_level[k-1][0][j][i] -= learn_rate * d_L_d_ypred * d_ypred_d_w
+                        d_ypred_d_b = deriv_sigmoid(self.forward_sum_value[k][j])
+                        #print('check',self.layers_level[k-1][1][j])
+                        #update bias
+                        self.layers_level[k-1][1][j] -= learn_rate * d_L_d_ypred * d_ypred_d_b
 
             if epoch % 10 == 0:
                 y_preds = np.apply_along_axis(self.feedforward, 1, data)
                 loss = mse_loss(ally_true, y_preds)
-                print("Epoch %d learning rate: %.3f" % (epoch, loss))
+                print("Epoch %d loss rate: %.3f" % (epoch, loss))
 
 
 if __name__ == "__main__":
